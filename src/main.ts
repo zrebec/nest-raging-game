9d1ae01b77b871e9dc2ca2bd469e1c17fd465016
zrebec <zrebec@zrebec.sk>
2023-07-25 16:56:39 +0200

    feat: creating controllers and dto fro drivers

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap() {
+  // Builing application to swagger
+  const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Racing Game')
+    .setDescription('Simuliation of racing event')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  // This means that every DTO (object) will use validation if class-validatorm is declared
+  app.useGlobalPipes(new ValidationPipe());
+
+  await app.listen(3000);
+}
+bootstrap();
